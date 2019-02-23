d1c7b6d2864c776c4222c7a05e727c7d6b2abb0f
Pawel Pisarski <ppisarski@pjwstk.edu.pl>
2019-02-23 13:53:22 +0100

    drugi kommit testowy

--- src/poj1/Poj1.java
+++ src/poj1/Poj1.java
@@ -15,7 +15,7 @@ public class Poj1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("jestem na branchu testowym");
+        System.out.println("jestem na branchu testowym, druga zmiana");
         // TODO code application logic here
     }
     
